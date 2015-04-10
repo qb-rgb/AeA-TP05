f6ca14fea4b112e2d90d2557d9438fb9e71ed18d
qw0te <baert.quentin@gmail.com>
2015-04-10 11:11:25 +0200

    Ajoute une classe Color pour représenter une couleur lors de la coloration d'un graphe

---
+++ src/main/scala/coloration/Color.scala
@@ -0,0 +1,10 @@
+/**
+ * Représente une couleur lors de la coloration d'un graphe
+ *
+ * @author Quentin Baert
+ */
+class Color(val id: Int) {
+
+  override def toString: String = "Color " + this.id
+
+}
