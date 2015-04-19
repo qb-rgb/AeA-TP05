ae96998f91d5c7265df6075e0a105d5badd3265c
qw0te <baert.quentin@gmail.com>
2015-04-19 16:20:05 +0200

    Ajoute de la ScalaDoc

--- src/main/scala/coloration/ColorationResult.scala
+++ src/main/scala/coloration/ColorationResult.scala
@@ -1,6 +1,10 @@
 /**
  * Représente le résultat d'une coloration de graphe
  *
+ * @constructor Construit un nouveau résultat de coloration
+ * @param verticesColor association sommet -> couleur donnée par la coloration
+ * @param nbOfColors nombre de couleurs différentes de la coloration
+ *
  * @author Quentin Baert
  */
 class ColorationResult(val graph: Graph, val verticiesColor: Map[Vertex, Color], val nbOfColors: Int) {
